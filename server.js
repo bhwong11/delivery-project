93b7c3ed9e01a9f4d4613440dc2de9b840b7ea89
austinschroeder <austinsoundy@gmail.com>
2021-12-14 14:05:17 -0800

    fix merge conflict

--- server.js
+++ server.js
@@ -2,17 +2,11 @@ if (process.env.NODE_ENV !== `production`) {
   require(`dotenv`).config();
 }
 
-<<<<<<< HEAD
 const express = require(`express`);
 const app = express();
 const expressLayouts = require(`express-ejs-layouts`);
-=======
-const express = require(`express`)
-const app = express()
-const expressLayouts = require(`express-ejs-layouts`)
-const session = require("express-session");
-const MongoStore = require("connect-mongo");
->>>>>>> origin
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 
 const indexRouter = require(`./routes/index`);
 
@@ -22,37 +16,31 @@ app.set(`layout`, `layouts/layout`);
 app.use(expressLayouts);
 app.use(express.static(`public`));
 
-<<<<<<< HEAD
+//Express session middleware
+app.use(
+  session({
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI || 'mongodb://localhost:27017/hackathon',
+    }),
+    secret: 'secretThatWillGoInENVWhenNotLazy',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7 * 2, // two weeks
+    },
+  })
+);
+
+app.use(function (req, res, next) {
+  res.locals.user = req.session.currentUser;
+  next();
+});
+
 const mongoose = require(`mongoose`);
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on(`error`, (error) => console.error(error));
 db.once(`open`, () => console.log(`Connection Established`));
-=======
-//Express session middleware
-app.use(
-    session({
-      store: MongoStore.create({ mongoUrl: process.env.MONGO_URI || 'mongodb://localhost:27017/hackathon' }),
-      secret: "secretThatWillGoInENVWhenNotLazy",
-      resave: false,
-      saveUninitialized: false,
-      cookie: {
-        maxAge: 1000 * 60 * 60 * 24 * 7 * 2, // two weeks
-      },
-    })
-);
-
-app.use(function (req, res, next) {
-    res.locals.user = req.session.currentUser;
-    next();
-  });
-
-const mongoose = require(`mongoose`)
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
-const db = mongoose.connection
-db.on(`error`, error => console.error(error))
-db.once(`open`, () => console.log(`Connection Established`))
->>>>>>> origin
 
 app.use(`/`, indexRouter);
 
