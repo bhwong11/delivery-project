27b9d7d33de52d0813f6685fc3f46e107d7d0350
bhwong11 <user@email.com>
2021-12-14 17:38:57 -0500

    pull check

--- routes/auth.js
+++ routes/auth.js
@@ -3,10 +3,49 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const {User} = require('../models');
 
-router.post('/register',async(req,res,next)=>{
+router.get('/index',(req,res,next)=>{
+    res.render('index');
+})
+
+router.get('/signup1',(req,res,next)=>{
+    res.render('signup/signup1');
+})
+
+router.get('/signup2',(req,res,next)=>{
+    res.render('signup/signup2');
+})
+
+router.get('/signup3',(req,res,next)=>{
+    res.render('signup/signup3');
+})
+
+router.post('/signup1',async(req,res)=>{
+    try{
+        req.session.userInfo = req.body
+        return res.redirect('/signup2')
+    }catch(err){
+        console.log(error.message)
+        return res.send(error.message)
+    }
+})
+
+router.post('/signup2',async(req,res)=>{
+    try{
+        req.session.userInfo = {
+            ...req.body,
+            ...req.session.userInfo,
+        }
+        return res.redirect('/signup2')
+    }catch(err){
+        console.log(error.message)
+        return res.send(error.message)
+    }
+})
+
+router.post('/signup3',async(req,res,next)=>{
     try{
         //if user exist
-        const foundUser = await User.exists({$or:[{email:req.body.email},{username:req.body.username}]})
+        const foundUser = await User.exists({$or:[{email:req.session.userInfo.email},{username:req.body.username}]})
         if(foundUser){
             console.log('User already exist');
             return res.render("auth/register", {err: "User already exists"});
