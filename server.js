907c75b5bf87f4d3597fb91e3c12b77ac3e43c03
bhwong11 <user@email.com>
2021-12-15 17:25:18 -0500

    check

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-//if (process.env.NODE_ENV !== `production`) {
-//  require(`dotenv`).config();
-//}
-require(`dotenv`).config();
+if (process.env.NODE_ENV !== `production`) {
+  require(`dotenv`).config();
+}
+
 
 const express = require(`express`);
 const app = express();
